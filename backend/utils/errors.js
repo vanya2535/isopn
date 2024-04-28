56928739a9888e380b7852de0faaa692a8d71a14
i.desyatnikov <i.desyatnikov@idaproject.com>
2024-04-28 23:40:58 +0300

    Бэк вынесен в отдельную репу

--- backend/utils/errors.js
+++
@@ -1,3 +0,0 @@
-export const mapErrors = errors => ({
-    errors: errors.errors.map(({ path, msg }) => ({ path, msg })),
-});
--- backend/utils/jsonInterceptor.js
+++
@@ -1,9 +0,0 @@
-export const jsonInterceptor = (req, res, next) => {
-    const originalJson = res.json;
-
-    res.json = data => {
-        originalJson.call(res, { ...data, ...(req.user || {}) });
-    };
-
-    next();
-};
